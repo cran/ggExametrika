a44365305119cbd7e20813e65dbf285ecbae12b7
Koji Kosugi <kosugitti@gmail.com>
2026-06-15 05:10:11 +0000

    version 1.1.1

--- R/Biclustering.R
+++ R/Biclustering.R
@@ -96,8 +96,7 @@
 #'
 #' @seealso \code{\link{plotRRV_gg}}, \code{\link{plotFRP_gg}}, \code{\link{plotScoreField_gg}}
 #'
-#' @importFrom ggplot2 ggplot aes geom_line geom_point labs theme
-#'   scale_color_manual scale_x_continuous scale_y_continuous
+#' @importFrom ggplot2 ggplot aes geom_line geom_point labs theme scale_color_manual scale_x_continuous scale_y_continuous
 #' @importFrom ggrepel geom_text_repel
 #' @export
 
@@ -157,7 +156,7 @@ plotCRV_gg <- function(data,
   plot_data <- data.frame(
     field = rep(1:n_fld, each = n_cls),
     field_label = rep(paste0("F", 1:n_fld), each = n_cls),
-    value = as.vector(t(CRV)),
+    value = as.vector(CRV),
     class = factor(rep(paste0("C", 1:n_cls), times = n_fld), levels = paste0("C", 1:n_cls)),
     class_num = rep(1:n_cls, times = n_fld)
   )
@@ -306,8 +305,7 @@ plotCRV_gg <- function(data,
 #'
 #' @seealso \code{\link{plotCRV_gg}}, \code{\link{plotFRP_gg}}, \code{\link{plotScoreField_gg}}
 #'
-#' @importFrom ggplot2 ggplot aes geom_line geom_point labs theme
-#'   scale_color_manual scale_x_continuous scale_y_continuous
+#' @importFrom ggplot2 ggplot aes geom_line geom_point labs theme scale_color_manual scale_x_continuous scale_y_continuous
 #' @importFrom ggrepel geom_text_repel
 #' @export
 
@@ -367,7 +365,7 @@ plotRRV_gg <- function(data,
   plot_data <- data.frame(
     field = rep(1:n_fld, each = n_rank),
     field_label = rep(paste0("F", 1:n_fld), each = n_rank),
-    value = as.vector(t(RRV)),
+    value = as.vector(RRV),
     rank = factor(rep(paste0("R", 1:n_rank), times = n_fld), levels = paste0("R", 1:n_rank)),
     rank_num = rep(1:n_rank, times = n_fld) # For label display
   )
